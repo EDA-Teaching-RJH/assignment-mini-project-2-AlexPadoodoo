2d3534ee8d6b6c519ce0c79f1b680f8c37644fce
AlexPadoodoo <app10@kent.ac.uk>
2026-03-06 13:55:15 +0000

    created a class to store the name of the player and another class to set the players game score to zero.

--- Farkle Game/main.py
+++ Farkle Game/main.py
@@ -6,4 +6,11 @@ from farkle_lib import Validator, calculate_score #Imports the custom library fu
 class Entity:
     def __init__(self, name):
         self.name = name  #Stores the name of the player
-        
+
+# Subclass representing the human player
+
+class Player(Entity):
+    def __init__(self, name):
+        super().__init__(name)  #Sets a name
+        self.total_score = 0 #Sets the player's game score to zero
+
