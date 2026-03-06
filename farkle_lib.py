386aad1bf368aaa68ce3737da2b90af797c4938d
AlexPadoodoo <app10@kent.ac.uk>
2026-03-06 13:42:03 +0000

    Finished the calculate

--- farkle_lib.py
+++ farkle_lib.py
@@ -13,5 +13,12 @@ def calculate_score(dice):
             tier_multiplier = 2 ** (count - 3)
             score += three_score * tier_multiplier # Add the calculated triple and quadruple score
             scoring_dice_count += count # Marks all these dice as used for scoring
-
+        else:
+            if num == 1:
+                score += count * 100
+                scoring_dice_count += count #Marks the dice as used
+            elif num == 5:
+                score += count * 50
+                scoring_dice_count += count
+    return score, scoring_dice_count # Returns both the score and the number of dice used
             
