cab237f79e2f32b6c5d7f9347f8136b2bd97ae72
AlexPadoodoo <app10@kent.ac.uk>
2026-03-06 13:32:35 +0000

    Created the farkle library file and imported the regex library . Also started creating the function which implements farkle scoring rules

--- farkle_lib.py
+++ farkle_lib.py
@@ -1,2 +1,5 @@
 import re # Import the regular expression library for pattern matching
 from collections import Counter # Import Counter to easily count the dice occurences
+
+#Function to implement Farkle scoring rules...
+def calculate_score(dice):
